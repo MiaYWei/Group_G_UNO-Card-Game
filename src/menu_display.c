466b4f9a4b47154f0a88f312c668ed0f09d7eaed
Mia Wei <miawei.mwei@gmail.com>
2020-11-26 23:47:41 -0500

    Updated main menu display information. - Mia

--- src/menu_display.c
+++ src/menu_display.c
@@ -5,9 +5,9 @@ void start_screen(void)
 {
     printf("***************************************************************\n");
     printf("*                                                             *\n");
-    printf("*                 UNO EXPRESS Card Game                       *\n");
+    printf("*           WELCOME to UNO EXPRESS CARD GAME                  *\n");
     printf("*                                                             *\n");
-    printf("*                       Group G                               *\n");
+    printf("*               SYSC5709 Project Group G                      *\n");
     printf("*                                                             *\n");
     printf("***************************************************************\n");
 
@@ -20,31 +20,38 @@ void start_screen(void)
     printf("Enter your choice\n");
 }
 
-void rules(void)
+void display_rules(void)
 {
     char choice[5] = { 0 };
 
-    printf("UNO EXPRESS RULES: \n\n");
-    printf("Objective of the Game \n");
-    printf("************************\n");
-    printf("Be the first player to get rid of all of your cards in each round.\n\n");
-    printf("Play the Game\n");
-
-    printf("************************\n");
-    printf("1. Human player starts the game. \n");
-    printf("2. The program deals each player 5 cards when start a new game.\n");
-    printf("3. On your turn, you must match a card from your hand to the card on the top of the DISCARD pile, either by number, color or symbol.\n\n");
+    printf("\nRULES of UNO EXPRESS CARD GAME: \n");
+    printf("************************************************************************\n");
+    printf("Normal Cards: \n\n");
+    printf(" 10 Blue cards - 0 to 9\n");
+    printf(" 10 Green cards - 0 to 9\n");
+    printf(" 10 Red cards - 0 to 9\n");
+    printf(" 10 Yellow cards - 0 to 9\n\n");    
+    printf("************************************************************************\n");
     /*printf("Action Cards (Release 2)\n");
     printf("************************\n");
     printf("Draw One card - When you play this card, the next player must draw 1 card and miss their turn. This card may only be played on a matching color or on another Draw One card. \n");
     printf("Wild card - When you play this card, you get to choose the color that continues play (any color including the color in play before the Wild card was laid down).\n");
     printf("Wild Draw 2 card - When you play this card, you get to choose the color that continues play PLUS the next player must draw 2 cards from the DRAW pile and lose their turn.\n");
     printf("Skip card - When you play this card, the next player is skipped (loses their turn). This card may only be played on a matching color or on another Skip card. \n\n");*/
+    printf("Objective of the Game \n");
+    printf(" Be the first player to get rid of all of your cards in each turn.\n\n");
+    printf("************************************************************************\n");
+    printf("Play the Game\n");
+    printf(" 1. Human player starts the game. \n");
+    printf(" 2. The program deals each player 5 cards when start a new game.\n");
+    printf(" 3. On your turn, you must match a card from your hand to the card on the top of the DISCARD pile, either by number, color or symbol.\n");
+    printf("    EXAMPLE: If the card on the top of the DISCARD pile is a blue 7, the player must put down a blue card OR any color 7.\n\n");
+    //printf(" Alternatively, the player can put down a Wild card(See FUNCTIONS OF ACTION CARDS");
+    printf("************************************************************************\n");
     printf("Win the Game\n");
-    printf("************************\n");
-    printf("Once a player has no cards left, the game is over. If no player is out of cards by the time the DRAW pile is depleted, the DISCARD pile is reshuffled and play continues.\n");
-    printf("\n\n");
-    printf("press any key to go back to menu...");
+    printf(" Once a player has no cards left, the game is over. The specific player is the winner of the game.\n");
+    printf(" If no player is out of cards by the time the DRAW pile is depleted, the DISCARD pile is reshuffled and play continues.\n");
+    printf("\n\npress any key to go back to menu...");
     scanf_s("%s", choice, 1);
 }
 
@@ -69,7 +76,7 @@ void main_menu(void)
                 start_new_game();
                 return;
             case 2:
-                rules();
+                display_rules();
                 break;
             case 3:
                 confirm_exit();
