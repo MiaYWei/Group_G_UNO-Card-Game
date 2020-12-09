46a4f58826586b583e62a67f548e2308624eb9f3
Mia Wei <miawei.mwei@gmail.com>
2020-12-08 23:32:59 -0500

    cleanup code

--- test/src/test_game.h
+++ test/src/test_game.h
@@ -1,5 +1,5 @@
 #ifndef __TEST_GAME_HEADER__
-#define __TEST_GAME_MANAGEMENT_HEADER__
+#define __TEST_GAME_HEADER__
 
 void test_game(void);
-#endif // __TEST_GAME_MANAGEMENT_HEADER__
+#endif // __TEST_GAME_HEADER__
