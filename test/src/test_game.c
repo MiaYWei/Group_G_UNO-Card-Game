75034bdb89cff9b69ddff793231726683505d66f
Mia Wei <miawei.mwei@gmail.com>
2020-12-17 12:10:55 -0500

    Added test function: test_handle_computer_turn()

--- test/src/test_game.c
+++ test/src/test_game.c
@@ -9,14 +9,19 @@ void test_initialize_players(void);
 void test_if_end_game(void);
 void test_player_process_draw_one_card(void);
 void test_player_process_wild_draw_two_card(void);
+void test_handle_computer_turn(void);
+void test_handle_human_turn(void);
 
 void test_game(void)
 {
-    write_log("\n--------------- Start Game Module Test ---------------\n");
+    write_log("\n--------------- Start Test: Game Module ---------------\n\n");
     test_initialize_players();
     test_if_end_game();
     test_player_process_draw_one_card();
     test_player_process_wild_draw_two_card();
+    test_handle_computer_turn();
+    test_handle_human_turn();
+    write_log("\n--------------- End of Test: Game Module ---------------\n");
     
     return;
 }
@@ -114,5 +119,65 @@ void test_player_process_wild_draw_two_card(void)
         write_fail_log(" The diff value of player_process_wild_draw_two_card():\n", actual_diff, expected_diff);
     }
 
+    return;
+}
+
+void test_handle_computer_turn(void)
+{
+    // Case 1: No playable card to discard, end of turn, game continues.
+    g_card_on_table.color = BLUE;
+    g_card_on_table.name = EIGHT;
+    g_player_on_turn = COMPUTER;
+
+    PlayerType_e expected = HUMAN;
+    g_players[COMPUTER].cards_on_hand = create_test_list(RED, ONE, FOUR);
+    g_draw_pile = create_test_list(YELLOW, FIVE, NINE);
+
+    handle_computer_turn();
+    PlayerType_e actual = g_player_on_turn;
+    if (expected == actual) {
+        write_log("Test-- - handle_computer_turn().Case 1:No Playable Card Discarded, End Turn.......Successful!\n");
+    }
+    else {
+        write_log("Test-- - handle_computer_turn().Case 1:Playable Card Discarded, End Turn......failed!\n");
+        write_fail_log(" The test value of handle_computer_turn():\n", actual, expected);
+    }
+
+    //Case 2: Playable card Discarded, End Turn
+    g_card_on_table.color = RED;
+    g_card_on_table.name = TWO;
+    g_player_on_turn = COMPUTER;
+    g_players[COMPUTER].cards_on_hand = create_test_list(RED, ONE, FOUR);
+
+    expected = HUMAN;
+
+    handle_computer_turn();
+    actual = g_player_on_turn;
+    if (expected == actual) {
+        write_log("Test-- - handle_computer_turn().Case 2:Playable Card Discarded, End Turn.......Successful!\n");
+    }
+    else {
+        write_log("Test-- - handle_computer_turn().Case 2:Playable Card Discarded, End Turn......failed!\n");
+        write_fail_log(" The test value of handle_computer_turn():\n", actual, expected);
+    }
+
+    // Case 3: Not Computer Turn.
+    g_player_on_turn = HUMAN;
+    expected = HUMAN;
+    handle_computer_turn();
+    actual = g_player_on_turn;
+    if (expected == actual) {
+        write_log("Test-- - handle_computer_turn().Case 3:Invalid Player.......Successful!\n");
+    }
+    else {
+        write_log("Test-- - handle_computer_turn().Case 3:Invalid Player......failed!\n");
+        write_fail_log(" The test value of handle_computer_turn():\n", actual, expected);
+    }
+
+    return;
+}
+
+void test_handle_human_turn(void)
+{
     return;
 }
--- test/src/test_human_player.c
+++ test/src/test_human_player.c
@@ -6,8 +6,10 @@ void test_record_human_input(void);
 int test_hunman_player(void)
 {
     int result = 0;
-    write_log("\n--------------- Start Human Player Module Test ---------------\n");
+    write_log("\n--------------- Start Test: Human Player Module ---------------\n\n");
     test_record_human_input();  
+    write_log("\n--------------- End of Test: Human Player Module ---------------\n");
+
     return result;
 }
 
