2c5c664953f20dc1d62cd61d99e70ba2ff5d8ece
Mia Wei <miawei.mwei@gmail.com>
2020-12-16 21:37:43 -0500

    Code cleanup

--- src/human_player.c
+++ src/human_player.c
@@ -11,14 +11,14 @@
 /* Global variables */
 bool g_card_requested = false;
 
+ret_type_e handle_human_turn(void);
+ret_type_e record_human_input(void);
+Card_t map_user_input(const char* user_input);
 int request_card(PlayerType_e PlayerType, int no_of_cards);
 void invalid_card_warning(void);
-Card_t map_user_input(const char* user_input);
-ret_type_e record_human_input(void);
 void show_cards_assigned(Card_t assigned_card);
 bool is_valid_card(Card_t current_card);
 bool is_human_card(Card_t current_card);
-int quit_game(void);
 bool is_wild_card(const char* user_input);
 bool is_skip_card(const char* user_input);
 ret_type_e human_process_end_turn_request(void);
@@ -26,6 +26,7 @@ ret_type_e human_process_new_card_request(void);
 ret_type_e human_process_action_wild_card(void);
 ret_type_e human_process_skip_card(const char* user_input);
 ret_type_e human_process_normal_card(const char* user_input);
+int quit_game(void);
 
 /**
  * @brief Function called when the human player requests a card during each turn/
