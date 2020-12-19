0c90c81a92b8f134e1e4970e221d5142f142f227
Mia Wei <miawei.mwei@gmail.com>
2020-12-19 14:44:26 -0500

    updated print

--- src/game.c
+++ src/game.c
@@ -39,7 +39,7 @@ void start_new_game(void)
     printf("\n\n");
 
     while (true){
-        printf("Card on the table now: (%s, %s).\n", CARD_COLOR_STRING[g_card_on_table.color], CARD_NAME_STRING[g_card_on_table.name]);
+        printf("Card on the table now: (%s,%s).\n", CARD_COLOR_STRING[g_card_on_table.color], CARD_NAME_STRING[g_card_on_table.name]);
         printf("Current player is ");
         print_info(PLAYER_TYPE_STRING[g_player_on_turn]);
 
