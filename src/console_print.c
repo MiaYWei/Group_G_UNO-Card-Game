1fa9cb0b82dd4cf5c88e9e5d972d064629fac9f5
Mia Wei <miawei.mwei@gmail.com>
2020-12-19 02:19:46 -0500

    added doxygen notation

--- src/console_print.c
+++ src/console_print.c
@@ -1,31 +1,42 @@
 #include "../include/console_print.h"
 
+/**
+ * @brief Set the console colour 
+ *
+ * @param attributes console screen buffer attributes info
+ * @param color the expected color
+ */
 void set_console_colour(WORD* attributes, WORD color)
 {
-    CONSOLE_SCREEN_BUFFER_INFO Info;
-    HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
-    GetConsoleScreenBufferInfo(hStdout, &Info);
-    *attributes = Info.wAttributes;
-    SetConsoleTextAttribute(hStdout, color);
+    CONSOLE_SCREEN_BUFFER_INFO info;
+    HANDLE handle_stdout = GetStdHandle(STD_OUTPUT_HANDLE);
+    GetConsoleScreenBufferInfo(handle_stdout, &info);
+    *attributes = info.wAttributes;
+    SetConsoleTextAttribute(handle_stdout, color);
 }
 
-void reset_console_colour(WORD attributes)
-{
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), attributes);
-}
-
+/**
+ * @brief wraped print function for warning print. The color is red.
+ *
+ * @param string pointer to the specific string to print
+ */
 void print_warning(const char* string)
 {
     WORD attributes = 0;
     set_console_colour(&attributes, FOREGROUND_INTENSITY | FOREGROUND_RED);
     printf("%s\n", string);
-    reset_console_colour(attributes);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), attributes);
 }
 
+/**
+ * @brief wraped print function for info print. The color is blue.
+ *
+ * @param string string pointer to the specific string to print
+ */
 void print_info(const char* string)
 {
     WORD attributes = 0;
     set_console_colour(&attributes, FOREGROUND_INTENSITY | FOREGROUND_BLUE);
     printf("%s\n", string);
-    reset_console_colour(attributes);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), attributes);
 }
