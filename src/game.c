6aded61e2d265b03f9e7ad89cf61f6627dcb9acd
Mia Wei <miawei.mwei@gmail.com>
2020-12-19 20:16:40 -0500

    fixed misused input parameter.

--- src/game.c
+++ src/game.c
@@ -165,7 +165,7 @@ void end_turn(PlayerType_e player)
     }
     
     // Computer says UNO when only have one card on hand
-    if ((1 == get_pile_length(&g_players[player].cards_on_hand)) && (player == COMPUTER))
+    if ((1 == get_pile_length(g_players[player].cards_on_hand)) && (player == COMPUTER))
     {
         print_info("COMPUTER Palyer: UNO !!!\n");
     }
